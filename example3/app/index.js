479d2050eee92d3eae2cb65677e723c50f7a459f
kiba15@naver.com <kiba15ktoon>
2018-06-30 14:09:13 +0900

    실습 - Example 3 - Libaraies Code Splitting
     [3-1 : jqeury 로딩]
    
    https://github.com/joshua1988/LearnWebpack#example-3---webpack-resolve--plugins
    
    1. npm init -y 으로 package.json 생성
    
    2. npm 명령어로 loader & plugin 설치
       $ npm install webpack jquery  --save-dev
    
    3.  index.html  ,  app/index.js  셍성
    
    4. Add webpack.config.js

--- example3/app/index.js
+++ example3/app/index.js
@@ -1,6 +1,6 @@
 // #1 - Using NPM
-// var $ = require('jquery');
-// console.log("loaded jQuery version is " + $.fn.jquery);
+var $ = require('jquery');
+console.log("loaded jQuery version is " + $.fn.jquery);
 
 // #2 - Using alias
 // import $ from 'Vendor/jquery-2.2.4.min.js';
