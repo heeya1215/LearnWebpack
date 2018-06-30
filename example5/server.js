1dd6159f97fb4c4287a8bbcd1a3bd26ea681d70f
kiba15@naver.com <kiba15ktoon>
2018-07-01 00:57:38 +0900

    실습 - Example 5-2 - Webpack Dev Middleware
    [실습] Webpack Dev Middleware

--- example5/server.js
+++ example5/server.js
@@ -2,6 +2,22 @@ var express = require('express');
 var app = express();  // express를 호출하여 app에 저장
 var path = require('path');
 
+// #1
+var webpackDevMiddleware = require("webpack-dev-middleware");
+var webpack = require("webpack");
+var webpackConfig = require("./webpack.config");
+var compiler = webpack(webpackConfig);
+
+// #2
+app.use(webpackDevMiddleware(compiler, {
+  // webpack.config.js의 publicPath를 활용한다고 지정,
+  publicPath: webpackConfig.output.publicPath,
+  // log의 색상을 지정
+  stats: {colors: true},
+  // 파일을 변경하였을때 즉시 적용되지 않고, Client의 요청을 하였을때 번들링됨
+  lazy: true
+}));
+
 app.use(express.static(__dirname));
 
 // view engine setup
